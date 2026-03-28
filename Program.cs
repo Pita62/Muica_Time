b763014ddcafd61e2eee202a07eeff720a26104f
Meu Nome <meu-email@meu-servidor.comguaru.guerreiro@gmail.com>
2026-03-27 22:42:52 -0300

    Atividade Musica

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿Console.ForegroundColor = ConsoleColor.Green;
+Console.BackgroundColor = ConsoleColor.White;
+Console.WriteLine("Hino do Palmeiras");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("Quando surge o alviverde imponente");
+
+Console.WriteLine("No gramado em que a luta o aguarda");
+
+Console.WriteLine("Sabe bem o que vem pela frente");
+
+Console.WriteLine("Que a dureza do prélio não tarda!");
+
+Console.WriteLine("E o Palmeiras no ardor da partida");
+
+Console.WriteLine("Transformando a lealdade em padrão");
+
+Console.WriteLine("Sabe sempre levar de vencida");
+
+Console.WriteLine("E mostrar que, de fato, é campeão!");
+
+Console.WriteLine("Defesa que ninguém passa!");
+
+Console.WriteLine("Linha atacante de raça e vibração");
+
+Console.WriteLine("Torcida que canta e vibra");
+
+Console.WriteLine("Por nosso alviverde inteiro");
+
+Console.WriteLine("Que sabe ser brasileiro");
+
+Console.WriteLine("Ostentando a sua fibra!");
+
+Console.WriteLine("Palmeiras! Pmeiras! Palmeiras!");
+Console.ResetColor();
